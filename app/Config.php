f0cf27d3325a3e29fd0bee481dee137188a62f2a
JoseRuiz00 <94843559+JoseRuiz00@users.noreply.github.com>
2021-11-30 14:02:41 +0100

    Cambio el config.php

--- app/Config.php
+++ app/Config.php
@@ -3,9 +3,9 @@
  class Config
  {
      static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentos2020";
-     static public $mvc_bd_usuario  = "myjesus95";
-     static public $mvc_bd_clave    = "Q1GuQDIX";
+     static public $mvc_bd_nombre   = "alimentosg12021";
+     static public $mvc_bd_usuario  = "usuJoseam2MySql";
+     static public $mvc_bd_clave    = "*usuario_CDMON1*";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
